f65930334d99c0a5791e241e9edb42dacadb0fb0
Nida Akalp <nidakalp2002@gmail.com>
2023-12-28 02:24:51 +0300

    arranged am-pm calculation and added commend line

--- Digital Clock/dig_clock.js
+++ Digital Clock/dig_clock.js
@@ -1,8 +1,8 @@
 getTime()
-// let changedParamater= document.querySelector('.am_pm')
+let changedParamater = document.querySelector('.am_pm')
 // changedParamater.innerHTML=`<h2>AM</h2>`
 function getTime() {
-  //data of hours
+  //Get data of hours
   let hours = document.querySelector('.hours')
   let newHour = new Date().getHours()
   if (newHour < 10) {
@@ -13,14 +13,14 @@ function getTime() {
     changedParamater.innerHTML = `<h2>PM</h2>`
   }
   hours.innerHTML = `<h3>${newHour}</h3>`
-  //data of minutes
+  //Get data of minutes
   let minutes = document.querySelector('.minutes')
   let newMinutes = new Date().getMinutes()
   if (newMinutes < 10) {
     newMinutes = '0' + newMinutes
   }
   minutes.innerHTML = `<h3>${newMinutes}</h3>`
-  //data of seconds
+  //Get data of seconds
   let seconds = document.querySelector('.seconds')
   let newSeconds = new Date().getSeconds()
   if (newSeconds < 10) {
