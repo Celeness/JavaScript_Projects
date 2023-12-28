2fc8e17beede5584f0df31cae9e6c1be1f781cef
Nida Akalp <nidakalp2002@gmail.com>
2023-12-28 18:48:41 +0300

    code sequence modified

--- Digital Clock/dig_clock.js
+++ Digital Clock/dig_clock.js
@@ -1,33 +1,42 @@
-getTime()
-let changedParamater = document.querySelector('.am_pm')
-// changedParamater.innerHTML=`<h2>AM</h2>`
 function getTime() {
-  //Get data of hours
+  //Get tag of parameter
   let hours = document.querySelector('.hours')
+  let minutes = document.querySelector('.minutes')
+  let seconds = document.querySelector('.seconds')
+
+  //Get data of date
   let newHour = new Date().getHours()
-  if (newHour < 10) {
-    newHour = '0' + newHour
-  }
+  let newMinutes = new Date().getMinutes()
+  let newSeconds = new Date().getSeconds()
+
+  //Check AM or PM
+  let changedParamater = document.querySelector('.am_pm')
   if (newHour >= 12) {
     newHour = newHour - 12
     changedParamater.innerHTML = `<h2>PM</h2>`
   }
-  hours.innerHTML = `<h3>${newHour}</h3>`
-  //Get data of minutes
-  let minutes = document.querySelector('.minutes')
-  let newMinutes = new Date().getMinutes()
+
+  //Add zero if one digit
+  if (newHour < 10) {
+    newHour = '0' + newHour
+  }
   if (newMinutes < 10) {
     newMinutes = '0' + newMinutes
   }
-  minutes.innerHTML = `<h3>${newMinutes}</h3>`
-  //Get data of seconds
-  let seconds = document.querySelector('.seconds')
-  let newSeconds = new Date().getSeconds()
   if (newSeconds < 10) {
     newSeconds = '0' + newSeconds
   }
+
+  //Print the screen
+  hours.innerHTML = `<h3>${newHour}</h3>`
+  minutes.innerHTML = `<h3>${newMinutes}</h3>`
   seconds.innerHTML = `<h3>${newSeconds}</h3>`
 }
+
+//Call get time function instantly
+getTime()
+
+// Call get time function every one second
 setInterval(() => {
   getTime()
 }, 1000)
