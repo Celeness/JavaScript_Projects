292c58aa22dac2cfadb43ae8abcb9b21bc75b876
Nida Akalp <nidakalp2002@gmail.com>
2024-01-01 15:17:38 +0300

    added keypress and backspace listener

---
+++ Real Time Char Counter/charCounter.js
@@ -0,0 +1,15 @@
+$('.input').keypress(function () {
+  let userText = $('.input').val()
+  let a = userText.length
+  $('.character span').text(a)
+  $('.remaining span').text(50 - a)
+})
+$('.input').keydown(function (event) {
+  let key = event.key
+  if (key === 'Backspace') {
+    let userText = $('.input').val()
+    let a = userText.length
+    $('.character span').text(a)
+    $('.remaining span').text(50 - a)
+  }
+})
