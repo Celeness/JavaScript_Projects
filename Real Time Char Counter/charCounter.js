d7c26e7042efd8b55f70433470d8aa83f558011e
Nida Akalp <nidakalp2002@gmail.com>
2024-01-02 00:49:12 +0300

    keypress listener changed with keyup

--- Real Time Char Counter/charCounter.js
+++ Real Time Char Counter/charCounter.js
@@ -1,10 +1,6 @@
-$('.input').keydown(function () {
-  let userText = $('.input').val()
-  let a = userText.length
-  $('.character span').text(a)
-  $('.remaining span').text(50 - a)
-})
-$('.input').keydown(function (event) {
+// KeyDown occurs when the user presses a key.
+// KeyUp occurs when the user releases a key
+$('.input').keyup(function (event) {
   let key = event.key
   if (key === 'Backspace') {
     let userText = $('.input').val()
@@ -12,4 +8,10 @@ $('.input').keydown(function (event) {
     $('.character span').text(a)
     $('.remaining span').text(50 - a)
   }
+})
+$('.input').keyup(function () {
+  let userText = $('.input').val()
+  let a = userText.length
+  $('.character span').text(a)
+  $('.remaining span').text(50 - a)
 })
